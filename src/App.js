54266d4366cd60bb87885b7f998d8e2335cc42b2
John Ahn <69434435+john-yo-ahn@users.noreply.github.com>
2021-06-27 11:46:06 -0400

    aADD: add blank components for website population

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import Home from "./components/home"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-        <h1>hello</h1>
-      </header>
+    <div>
+      <div className="App">
+        <header className="App-header">
+          <h1>Hi How are you?</h1>
+        </header>
+      </div>
+      <div>
+        <Home />
+      </div>
+      <div>
+        
+      </div>
     </div>
   );
 }
---
+++ src/components/home.js
@@ -0,0 +1,26 @@
+import * as React from 'react'
+import "../styles/App.css"
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+        <h1>Home</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
