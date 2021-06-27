f20089e87d2e6765aa83d002779e52cc2a20a5e7
John Ahn <69434435+john-yo-ahn@users.noreply.github.com>
2021-06-27 16:40:11 -0400

    ADD: fixed navigational issues + body size + flex

--- src/components/greeting.js
+++ src/components/greeting.js
@@ -5,20 +5,8 @@ function Greeting() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
         <h1>Greeting</h1>
-        <p>
+        <p class="App-body">
           The navbar will stick to the top when you reach its scroll position.
           Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum
           definitiones no quo, maluisset concludaturque et eum, altera fabulas
