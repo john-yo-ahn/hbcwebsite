88201af45c7503fcf8e1365013a7ae5819d61792
John Ahn <69434435+john-yo-ahn@users.noreply.github.com>
2021-06-27 15:10:20 -0400

    ADD: navbar added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './styles/App.css';
 
+import Navbar from './components/navbar';
 import Home from "./components/home"
 import Greeting from "./components/greeting"
 import About from "./components/about";
@@ -11,24 +12,27 @@ import Contact from "./components/contact"
 function App() {
   return (
     <div>
-      <div>
-        <Home />
-      </div>
-      <div>
-        <About />
-      </div>
-      <div>
-        <Greeting />
-      </div>
-      <div>
-        <Sermon />
-      </div>
-      <div>
-        <Donations />
-      </div>
-      <div>
-        <Contact />
-      </div>
+      <Navbar />
+      {/* <div>
+        <div>
+          <Home />
+        </div>
+        <div>
+          <About />
+        </div>
+        <div>
+          <Greeting />
+        </div>
+        <div>
+          <Sermon />
+        </div>
+        <div>
+          <Donations />
+        </div>
+        <div>
+          <Contact />
+        </div>
+      </div> */}
     </div>
   );
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,12 +1,94 @@
 import * as React from "react";
 import "../styles/App.css";
+import "../styles/navbar.css"
+
+
+import Home from "../components/home"
+import About from "../components/about"
+import Greeting from "../components/greeting"
+import Sermon from "../components/sermon"
+import Donations from "../components/donations"
+import Contact from "../components/contact"
+// import Home from "./components/home";
+// import Greeting from "./components/greeting";
+// import About from "./components/about";
+// import Donations from "./components/donations";
+// import Sermon from "./components/sermon";
+// import Contact from "./components/contact";
 
 function Navbar() {
   return (
-      <nav id="navbar-example2" class="navbar navbar-light bg-light px-3">
-          <a class="navbar-brand" href="#">Navbar</a>
-      </nav>
-  )
+    <div>
+      <div>
+        <nav
+          id="navbar-example2"
+          class="navbar navbar-light fixed-top bg-light px-3"
+        >
+          <a class="navbar-brand" href="#Home">
+            Holy City Bethesda Church
+          </a>
+          <ul class="nav nav-pills">
+            <li class="nav-item">
+              <a class="nav-link" href="#Home">
+                Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#About">
+                About
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#Greeting">
+                Greeting
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#Sermon">
+                Sermon
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#Donations">
+                Donations
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#Contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div
+        data-bs-spy="scroll"
+        data-bs-target="#navbar-example2"
+        data-bs-offset="0"
+        class="scrollspy-example"
+        tabindex="0"
+      >
+        <div id="Home">
+          <Home />
+        </div>
+        <div id="About">
+          <About />
+        </div>
+        <div id="Greeting">
+          <Greeting />
+        </div>
+        <div id="Sermon">
+          <Sermon />
+        </div>
+        <div id="Donations">
+          <Donations />
+        </div>
+        <div id="Contact">
+          <Contact />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
